500bd0dd03073e7e07fbf30de59648f3ef4bc0c5
Anthony <anthony.jimenezb@gmail.com>
2015-04-25 16:58:33 -0400

    Adding sound to drum collision.

--- Unity_Zigfu_Kinect/Assets/CollidePlz.cs
+++ Unity_Zigfu_Kinect/Assets/CollidePlz.cs
@@ -18,5 +18,12 @@ public class CollidePlz : MonoBehaviour {
 		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
 		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
 		print("Their relative velocity is " + collisionInfo.relativeVelocity);
+
+		if (collisionInfo.relativeVelocity.x != 0 ||
+		    collisionInfo.relativeVelocity.y != 0 ||
+		    collisionInfo.relativeVelocity.z != 0)
+		{
+			audio.Play ();
+		}
 	}
 }
