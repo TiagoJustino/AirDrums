6afe7529850bafc46e1014e6fb73f09333be5fd3
Anthony <anthony.jimenezb@gmail.com>
2015-05-06 16:03:12 -0400

    Enhanced collisions and hi-hat collider.

--- Unity_Zigfu_Kinect/Assets/CollidePlz.cs
+++ Unity_Zigfu_Kinect/Assets/CollidePlz.cs
@@ -15,15 +15,12 @@ public class CollidePlz : MonoBehaviour {
 
 	void OnCollisionEnter(Collision collisionInfo)
 	{
-		print("Detected collision between " + gameObject.name + " and " + collisionInfo.collider.name);
-		print("There are " + collisionInfo.contacts.Length + " point(s) of contacts");
-		print("Their relative velocity is " + collisionInfo.relativeVelocity);
 
-		if (collisionInfo.relativeVelocity.x != 0 ||
-		    collisionInfo.relativeVelocity.y != 0 ||
-		    collisionInfo.relativeVelocity.z != 0)
+		if (collisionInfo.relativeVelocity.magnitude != 0)
 		{
-			audio.Play ();
+			//If the hit is going downwards
+			if(collisionInfo.relativeVelocity.y < 0)
+				audio.Play ();
 		}
 	}
 }
