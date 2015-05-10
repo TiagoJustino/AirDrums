0f6f68fef8b20039484c9a4e5145b6d4329d6dcd
DhananjaiH <ndhaijaan@gmail.com>
2015-05-09 20:27:18 -0400

    Added VR Drums project folder
    Unity project folder for VR Drums. Files and objects in the Assets
    folder need to be organised.

---
+++ VR Drums/Assets/CollidePlz2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollidePlz2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+
+		if (collisionInfo.relativeVelocity.magnitude != 0)
+		{
+			//If the hit is going downwards
+			if(collisionInfo.relativeVelocity.y < 0)
+				audio.Play ();
+		}
+	}
+}
---
+++ VR Drums/Assets/Scripts/Webcam.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour 
+{
+
+	public MeshRenderer[] UseWebcamTexture;
+	private WebCamTexture webcamTexture;
+
+	// Use this for initialization
+	void Start () 
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		webcamTexture = new WebCamTexture();
+		//webcamTexture.deviceName = "USB Video Device";
+		print ("devices: " + devices.Length);
+		for(int i=0;i<devices.Length;i++)
+		{
+			print("devices: "+devices[i].name);
+		}
+		if (devices.Length > 1)
+		{
+			webcamTexture.deviceName = devices [1].name;
+		}
+		else
+		{
+			webcamTexture.deviceName = devices [0].name;
+		}
+
+		foreach(MeshRenderer r in UseWebcamTexture)
+		{
+			r.material.mainTexture = webcamTexture;
+		}
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	void OnGUI()
+	{
+		if (webcamTexture.isPlaying)
+		{
+			if (GUILayout.Button("Pause"))
+			{
+				webcamTexture.Pause();
+			}
+			if (GUILayout.Button("Stop"))
+			{
+				webcamTexture.Stop();
+			}
+		}
+		else
+		{
+			if (GUILayout.Button("Play"))
+			{
+				webcamTexture.Play();
+			}
+		}
+	}
+}
